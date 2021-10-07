4ce1f1690c20472750087d550f56f02ae66a7c98
Briam Flores <briam.flores@enkoder.com.mx>
2021-10-07 13:27:18 -0500

    feat: files fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,21 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
-export default {
-	kit: {
-		paths: {
-			base: 'https://briamft.github.io/PokedexSvelte/',
-		},
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		appDir: 'internal',
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+ // Consult https://github.com/sveltejs/svelte-preprocess
+ // for more information about preprocessors
+ preprocess: preprocess(),
+
+ kit: {
+ // hydrate the <div id="svelte"> element in src/app.html
+  target: '#svelte',
+  adapter: adapter({
+    pages: 'build',
+    assets: 'build',
+    fallback: null
+  })
+ }
 };
+
+export default config;
