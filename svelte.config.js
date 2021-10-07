7fc95fa10e7303c4ad3f504214d0704d240f4e35
Briam Flores <briam.flores@enkoder.com.mx>
2021-10-07 12:44:31 -0500

    fix: Changes in config file

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,15 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
 	kit: {
+		paths: {
+			base: 'https://github.com/BriamFT/PokedexSvelte.git',
+		},
 		adapter: adapter({
 			// default options are shown
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		})
+		}),
+		appDir: 'internal',
 	}
 };
