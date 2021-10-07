9be43fdb710c0dd5782060ae99e188f38c584ec0
Briam Flores <briam.flores@enkoder.com.mx>
2021-10-07 14:37:50 -0500

    fix: config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,22 @@
-import sveltePreprocess from 'svelte-preprocess';
+import preprocess  from 'svelte-preprocess';
 // import node from '@sveltejs/adapter-node';
-import static_adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+	preprocess: preprocess(),
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
 		//adapter: node(),
-		adapter: static_adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 
 		// Comment the paths if wants to run in dev mode.
 		paths: {
