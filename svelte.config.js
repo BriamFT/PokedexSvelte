5114dcf42ee9ffd9edfb9358110cfc809da1126c
Briam Flores <briam.flores@enkoder.com.mx>
2021-10-18 17:11:34 -0500

    configuration file

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,8 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess({
+		defaults: {markup: 'html', script: 'javascript', style: 'css'},
+		sourceMap: true,
         postcss: {
             plugins: [
                 tailwind, 
